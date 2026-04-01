ee3d0924ba5f0ee4fd1fbf0ec74e80bfef0e5dbd
kevinyap24 <kevinyap24@outlook.com>
2026-04-01 18:45:11 +0800

    Commit combining EDA and FC Shiny App.

--- cpi-shiny/app.R
+++ cpi-shiny/app.R
@@ -1,5 +1,10 @@
-pacman::p_load(shiny, tidyverse, plotly, timetk, modeltime, tidymodels, lubridate)
+pacman::p_load(
+  shiny, tidyverse, plotly, timetk, modeltime, tidymodels, lubridate,
+  DT, htmltools, bslib, reactable, reactablefmtr, bsicons,
+  shinycssloaders, shinyWidgets, readr
+)
 
+source("modules/mod_eda.R")
 source("modules/mod_forecast.R")
 source("ui.R")
 source("server.R")
--- cpi-shiny/ui.R
+++ cpi-shiny/ui.R
@@ -1,9 +1,18 @@
+source("modules/mod_eda.R")
 source("modules/mod_forecast.R")
 
 ui <- navbarPage(
   title = "SG CPI Dashboard",
-  # YQ and JH to add their tabs here later:
-  # tabPanel("EDA", eda_ui("eda")),
-  # tabPanel("Decomposition", decomp_ui("decomp")),
+  theme = bslib::bs_theme(
+    base_font = bslib::font_google("IBM Plex Sans"),
+    code_font = bslib::font_google("IBM Plex Mono"),
+    bg         = "#f5f0e8",
+    fg         = "#1a1612",
+    primary    = "#c0392b",
+    secondary  = "#3d3530"
+  ),
+  tabPanel("EDA",eda_ui("eda")),
   tabPanel("Forecasting", forecast_ui("forecast"))
+  # YQ and JH to add their tabs here later:
+  # tabPanel("Decomposition", decomp_ui("decomp"))
 )
